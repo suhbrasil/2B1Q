c917e033e74628f0507d73ed82974ceeb43bfade
Suzana <suzi.pachelle@gmail.com>
2023-11-19 18:07:40 -0300

    first commit

---
+++ receiver.py
@@ -0,0 +1,139 @@
+import socket
+import matplotlib.pyplot as plt
+import tkinter as tk
+
+# Cria um objeto de socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Define o endereço IP e a porta para o socket
+host = '192.168.15.91'  # Substitua pelo IP do servidor
+port = 12346  # A mesma porta usada pelo servidor
+
+# Conecta-se ao servidor
+client_socket.connect((host, port))
+
+# Função para converter de binário para ASCII
+def to_ascii(binary):
+    ascii_data = ''
+    string = ''.join(str(bit) for bit in binary)
+    for i in range(0, len(string), 8):
+        char = chr(int(string[i:i+8], 2))
+        ascii_data += char
+
+    return ascii_data
+
+# Função para aplicar o algoritmo de criptografia inverso
+def decode_2B1Q(data):
+    # Mapeamento inverso do algoritmo 2B1Q
+    signal_map_inverse = {
+        '+3': '00',
+        '+1': '01',
+        '-1': '10',
+        '-3': '11'
+    }
+
+    bits = []
+
+    # Estado inicial
+    state = '+3'
+
+    # Percorre os sinais quaternários
+    for signal in data:
+        # Verifica se o sinal está no mapeamento inverso
+        if signal in signal_map_inverse:
+            current_bits = signal_map_inverse[signal]
+        else:
+            # Em caso de sinal não reconhecido, mantém os últimos bits para evitar erros
+            current_bits = signal_map_inverse[state]
+
+        # Adiciona os bits atuais à lista de bits
+        bits.append(current_bits)
+
+        # Atualiza o estado para o próximo sinal
+        state = signal
+
+    return ''.join(bits)
+
+
+# Função para criar o gráfico
+def create_graph(data):
+    tuple_data = tuple(char for char in data)
+    x = list(range(len(tuple_data)))
+
+    # Configura posições dos elementos no eixo y
+    y_positions = {'-': 0, '0': 1, '+': 2}
+    y = [y_positions[str(value)] for value in tuple_data]
+
+    plt.step(x, y, where='post')
+    plt.yticks([0, 1, 2], ['-', '0', '+'])
+    plt.title('Sinal codificado')
+    plt.xlabel('Tempo')
+    plt.ylabel('Estado')
+    plt.show(block=False)
+
+# Função para receber os dados pelo socket
+def receive_data():
+    data = client_socket.recv(1024).decode()
+    return data
+
+def print_list(lista):
+    formatted_list = ', '.join(str(item) for item in lista)  # Convert list to a formatted string with comma separator
+    text_box.insert(tk.END, formatted_list)  # Insert the formatted list into the text field
+
+# Função chamada ao receber os dados
+def process_data(data):
+    # Cria o gráfico
+
+    create_graph(data)
+
+    text_box.insert(tk.END, "Mensagem recebida: " + data + "\n\n")
+
+    # Aplica o princípio do algoritmo de codificação de linha inverso
+    decoded_2b1q_data = decode_2B1Q(data)
+    text_box.insert(tk.END, "Mensagem decodificada em binário: ")
+    print_list(decoded_2b1q_data)
+    text_box.insert(tk.END, "\n\n")
+
+    # Aplica o algoritmo de criptografia inverso
+    ascii_data = to_ascii(decoded_2b1q_data)
+    text_box.insert(tk.END, "\n\nMensagem criptografada em ascii: " + ascii_data + "\n\n")
+
+    caser_decrypted_data = caeser_decrypt(ascii_data, 3, 0)
+    text_box.insert(tk.END, "\n\nMensagem descriptografada: " + caser_decrypted_data + "\n\n")
+
+    # Exibe a mensagem recebida
+    print('Mensagem descriptografada:', ''.join(caser_decrypted_data))
+
+    text_box.pack()
+
+def index_in_list(a_list, index):
+    return index < len(a_list)
+
+def caeser_decrypt(data, key, mode):
+    alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'
+    new_data = ''
+    for c in data:
+        index = alphabet.find(c)
+        if index == -1:
+            new_data += c
+        else:
+            new_index = index + key if mode == 1 else index - key
+            new_index = new_index % len(alphabet)
+            new_data += alphabet[new_index:new_index+1]
+    return new_data
+
+# Recebe os dados
+
+root = tk.Tk()
+root.title('Comunicação de Dados')
+text_box = tk.Text(root)
+
+data_received = receive_data()
+
+# Processa os dados
+process_data(data_received)
+
+input()
+
+# Fecha a conexão e o socket
+client_socket.close()
---
+++ sender.py
@@ -0,0 +1,159 @@
+import tkinter as tk
+import socket
+import matplotlib.pyplot as plt
+
+# Configurações de rede
+HOST = '127.0.0.1'  # Endereço IP do computador servidor
+PORT = 12346       # Porta para a conexão
+
+# Cria um objeto de socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Associa o socket ao endereço IP e à porta
+server_socket.bind((HOST, PORT))
+
+# Coloca o socket em modo de escuta
+server_socket.listen(1)
+
+print('Aguardando conexão...')
+
+# Aceita a conexão do cliente
+client_socket, addr = server_socket.accept()
+
+print('Conexão estabelecida de:', addr)
+
+# Função para aplicar o algoritmo 2B1Q
+def apply_2B1Q(data):
+    # Mapeamento do algoritmo 2B1Q para cada par de bits
+    signal_map = {
+        '00': '+3',
+        '01': '+1',
+        '10': '-1',
+        '11': '-3'
+    }
+
+    signal = []
+
+    # Estado inicial
+    state = '+3'
+
+    # Percorre os bits de 2 em 2
+    for i in range(0, len(data), 2):
+        pair = data[i:i + 2]
+
+        # Verifica se o par de bits está no mapeamento
+        if pair in signal_map:
+            current_signal = signal_map[pair]
+        else:
+            # Em caso de erro, mantém o último sinal para evitar transições abruptas
+            current_signal = state
+
+        # Adiciona o sinal atual à lista de sinais
+        signal.append(current_signal)
+
+        # Atualiza o estado para o próximo par de bits
+        state = current_signal
+
+    return ''.join(signal)
+
+
+# Função para converter ascii para binário
+def to_binary(ascii):
+    binary = ""
+    for valor in ascii:
+        binary += bin(valor)[2:].zfill(8)
+    return binary
+
+
+# Função para enviar os dados pelo socket
+def send_data(data):
+    encoded = data.encode()
+    client_socket.sendall(encoded)
+
+# Função para criar o gráfico
+def create_graph(data):
+    tuple_data = tuple(char for char in data)
+    x = list(range(len(tuple_data)))
+
+    # Configura posições dos elementos no eixo y
+    y_positions = {'-': 0, '0': 1, '+': 2}
+    y = [y_positions[str(value)] for value in tuple_data]
+
+    plt.step(x, y, where='post')
+    plt.yticks([0, 1, 2], ['-', '0', '+'])
+    plt.title('Sinal codificado')
+    plt.xlabel('Tempo')
+    plt.ylabel('Estado')
+    plt.show(block=False)
+
+# Função chamada ao clicar no botão
+def send_text():
+    text = entry.get()  # Obtém o texto digitado
+    text_box.insert(tk.END, "Mensagem original:" + text + "\n\n")
+
+    caesar_text = caesar_encrypt(text, 3, 1)
+    text_box.insert(tk.END, "Mensagem criptografada:" + caesar_text + "\n\n")
+
+    # Transforma em ascii estendido
+    ascii_text = ascii_encode(caesar_text)
+    text_box.insert(tk.END, "Mensagem criptografa em ascii:")
+    print_list(ascii_text)
+
+    # Converte para binário
+    binary_data = to_binary(ascii_text)
+    text_box.insert(tk.END, "\n\nAscii em binário:" + binary_data + "\n\n")
+
+    # Aplica a criptografia 2B1Q
+    data_2b1q = apply_2B1Q(binary_data)
+    text_box.insert(tk.END, "Binário codificado com 2B1Q:" + data_2b1q + "\n\n")
+
+    text_box.pack()
+
+    # Cria o gráfico
+    create_graph(data_2b1q)
+
+    # Envia os dados para o outro computador
+    send_data(data_2b1q)
+
+def ascii_encode(string):
+    ascii = []
+    for char in string:
+        ascii.append(ord(char))
+    return ascii
+
+def caesar_encrypt(data, key, mode):
+    alphabet = 'abcdefghijklmnopqrstuvwyzàáãâéêóôõíúçABCDEFGHIJKLMNOPQRSTUVWYZÀÁÃÂÉÊÓÕÍÚÇ'
+    new_data = ''
+    for c in data:
+        index = alphabet.find(c)
+        if index == -1:
+            new_data += c
+        else:
+            new_index = index + key if mode == 1 else index - key
+            new_index = new_index % len(alphabet)
+            new_data += alphabet[new_index:new_index+1]
+    return new_data
+
+def print_list(lista):
+    formatted_list = ', '.join(str(item) for item in lista)  # Convert list to a formatted string with comma separator
+    text_box.insert(tk.END, formatted_list)  # Insert the formatted list into the text field
+
+# Configuração da interface gráfica usando tkinter
+root = tk.Tk()
+root.title('Comunicação de Dados')
+
+label = tk.Label(root, text='Digite o texto:')
+label.pack()
+
+entry = tk.Entry(root, width=50)
+entry.pack()
+
+button = tk.Button(root, text='Enviar', command=send_text)
+button.pack()
+
+text_box = tk.Text(root)
+
+root.mainloop()
+
+# Fecha o socket
+server_socket.close()
