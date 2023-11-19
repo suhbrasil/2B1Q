e0d0c0d3fcc69af51c3cd2c864a5cf47e33ad54a
Suzana <suzi.pachelle@gmail.com>
2023-11-19 19:26:15 -0300

    Começando

--- receiver.py
+++ receiver.py
@@ -57,19 +57,20 @@ def decode_2B1Q(data):
 
 # Função para criar o gráfico
 def create_graph(data):
-    tuple_data = tuple(char for char in data)
+    tuple_data = tuple(data[i:i+2] for i in range(0, len(data), 2))  # Agrupa os dados em pares
     x = list(range(len(tuple_data)))
 
     # Configura posições dos elementos no eixo y
-    y_positions = {'-': 0, '0': 1, '+': 2}
+    y_positions = {"-3": 0, "-1": 2, "+1": 4, "+3": 6}
     y = [y_positions[str(value)] for value in tuple_data]
 
     plt.step(x, y, where='post')
-    plt.yticks([0, 1, 2], ['-', '0', '+'])
+    plt.yticks([0, 2, 4, 6], ['-3', '-1', '+1', '+3'])
     plt.title('Sinal codificado')
     plt.xlabel('Tempo')
     plt.ylabel('Estado')
     plt.show(block=False)
+
 
 # Função para receber os dados pelo socket
 def receive_data():
--- sender.py
+++ sender.py
@@ -72,15 +72,15 @@ def send_data(data):
 
 # Função para criar o gráfico
 def create_graph(data):
-    tuple_data = tuple(char for char in data)
+    tuple_data = tuple(data[i:i+2] for i in range(0, len(data), 2))  # Agrupa os dados em pares
     x = list(range(len(tuple_data)))
 
     # Configura posições dos elementos no eixo y
-    y_positions = {'-': 0, '0': 1, '+': 2}
+    y_positions = {"-3": 0, "-1": 2, "+1": 4, "+3": 6}
     y = [y_positions[str(value)] for value in tuple_data]
 
     plt.step(x, y, where='post')
-    plt.yticks([0, 1, 2], ['-', '0', '+'])
+    plt.yticks([0, 2, 4, 6], ['-3', '-1', '+1', '+3'])
     plt.title('Sinal codificado')
     plt.xlabel('Tempo')
     plt.ylabel('Estado')
